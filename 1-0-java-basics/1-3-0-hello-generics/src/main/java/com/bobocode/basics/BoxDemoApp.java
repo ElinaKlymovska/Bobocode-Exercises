c8d2aedc6caf1cfb2feba4e5603ea0db4dab8fcd
Elina Klymovska <klymovska.elina@gmail.com>
2022-07-11 18:00:00 +0300

    done task "Crazy Generics"

--- 1-0-java-basics/1-3-0-hello-generics/src/main/java/com/bobocode/basics/BoxDemoApp.java
+++ 1-0-java-basics/1-3-0-hello-generics/src/main/java/com/bobocode/basics/BoxDemoApp.java
@@ -14,7 +14,7 @@ public class BoxDemoApp {
         System.out.println((int) intBox.getValue() + (int) intBox2.getValue());
 
         intBox.setValue(222);
-        intBox.setValue("abc"); // this should not be allowed
+        //intBox.setValue("abc"); // this should not be allowed
         // the following code will compile, but will throw runtime exception
         System.out.println((int) intBox.getValue() + (int) intBox2.getValue());
     }
